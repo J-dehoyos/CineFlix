324fd049bacef1411efd74d5219ca661723684f3
J-dehoyos <jgdehoyos09@gmail.com>
2023-11-13 13:18:06 -0500

    Se crea la clase del objeto pelicula

--- src/cineflix/pelicula.java
+++ src/cineflix/pelicula.java
@@ -9,5 +9,47 @@ package cineflix;
  * @author jesus
  */
 public class pelicula {
+    String image, titulo, desc, precio;
+    pelicula sig;
+
+    public pelicula(String image, String titulo, String desc, String precio) {
+        this.image = image;
+        this.titulo = titulo;
+        this.desc = desc;
+        this.precio = precio;
+        sig = null;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
     
 }
